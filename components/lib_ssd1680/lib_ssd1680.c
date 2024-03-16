8e55d6285cea66e8f3ce7a4b6b42041081485d43
wrobepio <piotr.wrobel.stanislaw@gmail.com>
2024-03-16 20:41:36 +0100

    ssd1680: function set_area (6)

--- components/lib_ssd1680/lib_ssd1680.c
+++ components/lib_ssd1680/lib_ssd1680.c
@@ -371,7 +371,7 @@ void ssd1680_draw_line(ssd1680_t *disp, uint16_t x1, uint16_t y1, uint16_t x2, u
 void ssd1680_set_area(ssd1680_t *disp, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t* area, uint16_t area_size, ssd1680_color_t color)
 {
 
-	static const uint8_t BitsSetTable[8] = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F };
+	static const uint8_t BitsSetTable[8] = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F }, BitsSetTableRev[8] = { 0xFF, 0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01 };
 	int clmn_start, clmn_stop, idx; //offset;
 	uint8_t x1bits, x2bits;
 	uint16_t ycurr, xcurr;
@@ -442,7 +442,8 @@ void ssd1680_set_area(ssd1680_t *disp, uint16_t x1, uint16_t y1, uint16_t x2, ui
 					} else if (xcurr == clmn_stop && x2bits > 0 && x1bits > 0)
 					{
 						if(clmn_stop - clmn_start > 1)
-							disp->framebuffer_bw[idx] = (disp->framebuffer_bw[idx] | (uint8_t)~BitsSetTable[8-x2bits-x1bits]) & ~(~*(area - 1) & (uint8_t)~(BitsSetTable[8 - x1bits])) & (*area & ~(BitsSetTable[8 - x2bits])) >> x1bits; //!!!
+							//disp->framebuffer_bw[idx] = 0x00;
+							disp->framebuffer_bw[idx] = (disp->framebuffer_bw[idx] | (uint8_t)~BitsSetTableRev[x2bits]) & ((uint8_t)~((uint8_t)~*area  << x1bits) | BitsSetTableRev[x2bits]); //???
 						else
 							disp->framebuffer_bw[idx] = (disp->framebuffer_bw[idx] | BitsSetTable[8 - x1bits]) & ~(~*area << x1bits); //OK!
 					} else if ( clmn_start != clmn_stop && xcurr == clmn_stop && x2bits > 0)
