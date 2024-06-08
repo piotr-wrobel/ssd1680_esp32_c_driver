235480b68149f695d6460a2a9c06fed774c0b863
wrobepio <piotr.wrobel.stanislaw@gmail.com>
2024-06-08 22:34:57 +0200

    Tool to convert images to C array (7)

--- utils/compile.sh
+++ utils/compile.sh
@@ -2,5 +2,5 @@ rm -rf build
 mkdir -p build
 for I in *.c; do
 echo "compile $I..."
-gcc  -Wall "$I" -o "build/"`echo $I | sed -e 's/.c//g'`
+gcc -Wall  "$I" -o "build/"`echo $I | sed -e 's/.c//g'`
 done
--- utils/convert_image.c
+++ utils/convert_image.c
@@ -10,33 +10,73 @@ int main(int argc, char *argv[])
 	uint8_t * buff;
 	int width = 0, height = 0, pixels = 0, bit = 0;
 	char phraze1[255];
-	FILE *wp;
+	FILE * input_file;
+	FILE * output_file;
 
-	if(argc != 2)
+	if(argc != 3)
 	{
-		printf("Use: %s gimp_indexed_image_as_c_header_file.h\n", argv[0]);
+		printf("Use: %s gimp_indexed_image_as_c_header_file.h table_name\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	if ((wp = fopen(argv[1], "r")) == NULL)
+	if( strlen(argv[1]) > 200 )
+	{
+		printf("The length of the input file name is greater than 200 characters\r\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	printf("input: %s\r\n", argv[1]);
+	char input_file_name[255], input_path[255];
+	char * input_path_ptr;
+	strncpy(input_file_name, argv[1], strlen(argv[1]) + 1);
+	input_path_ptr = strrchr(input_file_name,'/');
+	if(input_path_ptr != NULL)
+	{
+		//printf("%d,%d\r\n",(int)input_path_ptr,(int)input_file_name);
+		printf("Path length: %d\r\n", (int)(input_path_ptr - input_file_name + 1));
+		strncpy(input_path, input_file_name, (int)(input_path_ptr - input_file_name + 1));
+	} else
+	{
+		input_path[0] = '\0';
+	}
+	printf("Path: %s\r\n", input_path);
+
+
+	if ((input_file = fopen(argv[1], "r")) == NULL)
 	{
 		printf("Cannot open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	rewind(wp);
+	rewind(input_file);
 
-	while(fscanf(wp, "%s", phraze1) == 1)
+
+	char table_name[11];
+	int table_name_length = strlen(argv[2]);
+	if(table_name_length > 10) table_name_length = 10;
+	printf("table_name_length: %d\r\n", table_name_length);
+	strncpy(table_name, argv[2], table_name_length + 1);
+
+
+
+
+//	printf("output: %s\r\n", argv[2]);
+//	printf("table: %s\r\n", argv[3]);
+
+
+
+	while(fscanf(input_file, "%s", phraze1) == 1)
 	{
-		if(!strcmp(phraze1,"width") && fscanf(wp,"%s",phraze1) && !strcmp(phraze1,"="))
+		if(!strcmp(phraze1,"width") && fscanf(input_file,"%s",phraze1) && !strcmp(phraze1,"="))
 		{
-			fscanf(wp,"%d",&width);
+			fscanf(input_file,"%d",&width);
 		}
-		if(!strcmp(phraze1,"height") && fscanf(wp,"%s",phraze1) && !strcmp(phraze1,"="))
+		if(!strcmp(phraze1,"height") && fscanf(input_file,"%s",phraze1) && !strcmp(phraze1,"="))
 		{
-			fscanf(wp,"%d",&height);
+			fscanf(input_file,"%d",&height);
 		}
 
-		if(!strcmp(phraze1,"header_data[]") && fscanf(wp,"%s",phraze1) && !strcmp(phraze1,"=") && fscanf(wp,"%s",phraze1) && !strcmp(phraze1,"{"))
+		if(!strcmp(phraze1,"header_data[]") && fscanf(input_file,"%s",phraze1) && !strcmp(phraze1,"=") && fscanf(input_file,"%s",phraze1) && !strcmp(phraze1,"{"))
 		{
 
 			printf("Found data !\n");
@@ -44,13 +84,18 @@ int main(int argc, char *argv[])
 			{
 				pixels = width * height;
 				buff = malloc(pixels);
+				if(buff == NULL)
+				{
+					printf("Cannot allocate memory!\r\n");
+					exit(EXIT_FAILURE);
+				}
 				printf("Image size: %d x %d, pixels: %d\n", width, height, pixels);
 
 				for(int i = 0; i < pixels; i++)
 				{
-					if (fscanf(wp,"%d,", &bit) == 1)
+					if (fscanf(input_file,"%d,", &bit) == 1)
 					{
-						printf("%d",bit);
+						//printf("%d",bit);
 
 					} else
 						bit = 0;
@@ -58,55 +103,79 @@ int main(int argc, char *argv[])
 				}
 			} else
 			{
-				printf("Patterns not exist\n");
+				printf("Patterns not exist\r\n");
 				exit(EXIT_FAILURE);
 			}
 		}
-   }
+	}
+	if( fclose(input_file) != 0 )
+		printf("Cannot close input file!\r\n");
 
+	printf("\r\n");
 
-uint8_t (* wsk)[width] = (uint8_t (*)[width])buff;
+	uint8_t (* wsk)[width] = (uint8_t (*)[width])buff;
 
-
-uint8_t dst_byte = 0,byte = 0,counter = 1;
-
-for(int j = 0; j < height + 1; j++)
-{
-	printf("//y = %d\r\n",j);
-	for(int i = 0; i < width; i++)
+	char output_file_name[255], strbuff[20];
+	strcpy(output_file_name, input_path);
+	strcat(output_file_name, "image_");
+	strcat(output_file_name, table_name);
+	strcat(output_file_name, "_");
+	sprintf(strbuff,"%d",width);
+	strcat(output_file_name, strbuff);
+	strcat(output_file_name, "_");
+	sprintf(strbuff,"%d",height);
+	strcat(output_file_name, strbuff);
+	strcat(output_file_name, ".h");
+	uint8_t dst_byte = 0,byte = 0,counter = 1;
+	if ((output_file = fopen(output_file_name, "w+")) == NULL)
 	{
-		byte = (*wsk)[i];
-		if(byte == 1)
-			dst_byte += 128;
-		if(((i + 1) % 8 > 0 || i == 0) && i != width - 1)
+		printf("Cannot open/create file %s\n", output_file_name);
+		exit(EXIT_FAILURE);
+	}
+	fprintf(output_file, "#include <stdint.h>\r\n\r\n");
+	fprintf(output_file, "uint8_t image_%s_%d_%d[] =\r\n{\r\n", table_name, width, height);
+
+	for(int j = 0; j < height + 1; j++)
+	{
+		fprintf(output_file, "\t//y = %d\r\n\t",j);
+		for(int i = 0; i < width; i++)
 		{
-			dst_byte = dst_byte >> 1 ;
-		}else
-		{
-
-			if(i == width - 1)
+			byte = (*wsk)[i];
+			if(byte == 1)
+				dst_byte += 128;
+			if(((i + 1) % 8 > 0 || i == 0) && i != width - 1)
 			{
-				dst_byte = dst_byte >> (8 - (width % 8));
-			}
-			printf("0x%.2x, ",dst_byte);
-			dst_byte = 0;
-
-			if(counter == 8)
+				dst_byte = dst_byte >> 1 ;
+			}else
 			{
-				printf("\r\n");
-				counter = 0;
-			}
-			counter++;
 
-			if(i == width - 1)
-			{
-				printf("\r\n");
+				if(i == width - 1)
+				{
+					dst_byte = dst_byte >> (8 - (width % 8));
+				}
+				fprintf(output_file, "0x%.2x, ",dst_byte);
+				dst_byte = 0;
+
+				if(counter == 8)
+				{
+					fprintf(output_file, "\r\n\t");
+					counter = 0;
+				}
+				counter++;
+
+				if(i == width - 1)
+				{
+					fprintf(output_file, "\r\n");
+				}
 			}
 		}
+		wsk++;
 	}
-	wsk++;
-}
+	fprintf(output_file, "}\r\n");
 
-free(buff);
+	free(buff);
+
+	if( fclose(output_file) != 0 )
+		printf("Cannot close output file file!\n");
 
 } 
